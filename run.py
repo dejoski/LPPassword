9b81ee2bb0420b4566b33ad40cebf9f173cf92f9
dejoski <dstajic@10jinsolutions.com>
2020-07-30 07:33:14 -0400

    sad

---
+++ run.py
@@ -0,0 +1,15 @@
+#%%
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods = ['GET', 'POST'])
+def hello():
+    return jsonify("hello")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
